02a11d5bcb8f60cda14fc186597ca4104aa5da60
BejenaruIoanMatei <bejematei@gmail.com>
2025-04-30 15:45:55 +0300

    3. Profile view and urls

--- friend_system/profiles/models.py
+++ friend_system/profiles/models.py
@@ -3,12 +3,33 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+STATUS_CHOICES = (
+    ('send', 'Send'),
+    ('accepted', 'Accepted'),
+)
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField()
     friends = models.ManyToManyField(User, related_name='friends', blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+    
+    def get_friends(self):
+        return self.friends.all()
+    
+    def get_friends_no(self):
+        return self.friends.all().count()
 
     def __str__(self):
         return str(self.user)
+    
+class Relationship(models.Model):
+    sender = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='sender')
+    receiver = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='receiver')
+    status = models.CharField(max_length=8, choices=STATUS_CHOICES)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.sender}-{self.receiver}-{self.status}"
